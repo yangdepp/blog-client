10733282b155397ac0bf614ade2555bc232a5bb9
yangdepp <965093975@qq.com>
2019-01-25 18:25:06 +0800

    init

---
+++ src/app.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div id="cover"></div>
+    <Header></Header>
+    <todo></todo>
+  </div>
+</template>
+<script>
+import Header from './todo/header.vue';
+import todo from './todo/todo.vue';
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Header,
+    todo
+  }
+};
+</script>
+
+<style lang="less" scoped>
+#app {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+#cover {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #999;
+  opacity: 0.9;
+  z-index: -1;
+}
+</style>
